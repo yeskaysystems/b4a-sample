187df7a419581d9b9c5ec49cc1a1546b9c70f9ac
Shervin Gharib <shervin.gharib@ronash.co>
2015-11-29 13:17:17 +0330

    b4a project added

---
+++ PusheSampleB4A/Objects/shell/src/co/ronash/pushesampleb4a/main_subs_6.java
@@ -0,0 +1,92 @@
+package co.ronash.pushesampleb4a;
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.pc.*;
+
+public class main_subs_6 {
+
+
+public static RemoteObject  _activity_create(RemoteObject _firsttime) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Create (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,34);
+if (RapidSub.canDelegate("activity_create")) return main.remoteMe.runUserSub(false, "main","activity_create", _firsttime);
+Debug.locals.put("FirstTime", _firsttime);
+ BA.debugLineNum = 34;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+Debug.ShouldStop(2);
+ BA.debugLineNum = 36;BA.debugLine="p.initialize(True)";
+Debug.ShouldStop(8);
+main.mostCurrent._p.runVoidMethod ("initialize",main.mostCurrent.activityBA,(Object)(main.mostCurrent.__c.getField(true,"True")));
+ BA.debugLineNum = 44;BA.debugLine="End Sub";
+Debug.ShouldStop(2048);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			Debug.ErrorCaught(e);
+			throw e;
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_pause(RemoteObject _userclosed) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Pause (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,50);
+if (RapidSub.canDelegate("activity_pause")) return main.remoteMe.runUserSub(false, "main","activity_pause", _userclosed);
+Debug.locals.put("UserClosed", _userclosed);
+ BA.debugLineNum = 50;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+Debug.ShouldStop(131072);
+ BA.debugLineNum = 52;BA.debugLine="End Sub";
+Debug.ShouldStop(524288);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			Debug.ErrorCaught(e);
+			throw e;
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_resume() throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Resume (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,46);
+if (RapidSub.canDelegate("activity_resume")) return main.remoteMe.runUserSub(false, "main","activity_resume");
+ BA.debugLineNum = 46;BA.debugLine="Sub Activity_Resume";
+Debug.ShouldStop(8192);
+ BA.debugLineNum = 48;BA.debugLine="End Sub";
+Debug.ShouldStop(32768);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			Debug.ErrorCaught(e);
+			throw e;
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _globals() throws Exception{
+ //BA.debugLineNum = 24;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 29;BA.debugLine="Dim p As Pushe";
+main.mostCurrent._p = RemoteObject.createNew ("co.ronash.pushe.b4awrapper.PusheWrapper");
+ //BA.debugLineNum = 32;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (main.processGlobalsRun == false) {
+	    main.processGlobalsRun = true;
+		try {
+		        main_subs_0._process_globals();
+starter_subs_0._process_globals();
+main.myClass = BA.getDeviceClass ("co.ronash.pushesampleb4a.main");
+starter.myClass = BA.getDeviceClass ("co.ronash.pushesampleb4a.starter");
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}public static RemoteObject  _process_globals() throws Exception{
+ //BA.debugLineNum = 18;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 22;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+}
---
+++ PusheSampleB4A/Objects/shell/src/co/ronash/pushesampleb4a/main_subs_9.java
@@ -0,0 +1,92 @@
+package co.ronash.pushesampleb4a;
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.pc.*;
+
+public class main_subs_9 {
+
+
+public static RemoteObject  _activity_create(RemoteObject _firsttime) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Create (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,35);
+if (RapidSub.canDelegate("activity_create")) return main.remoteMe.runUserSub(false, "main","activity_create", _firsttime);
+Debug.locals.put("FirstTime", _firsttime);
+ BA.debugLineNum = 35;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+Debug.ShouldStop(4);
+ BA.debugLineNum = 37;BA.debugLine="p.initialize(True)";
+Debug.ShouldStop(16);
+main.mostCurrent._p.runVoidMethod ("initialize",main.mostCurrent.activityBA,(Object)(main.mostCurrent.__c.getField(true,"True")));
+ BA.debugLineNum = 45;BA.debugLine="End Sub";
+Debug.ShouldStop(4096);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			Debug.ErrorCaught(e);
+			throw e;
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_pause(RemoteObject _userclosed) throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Pause (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,51);
+if (RapidSub.canDelegate("activity_pause")) return main.remoteMe.runUserSub(false, "main","activity_pause", _userclosed);
+Debug.locals.put("UserClosed", _userclosed);
+ BA.debugLineNum = 51;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+Debug.ShouldStop(262144);
+ BA.debugLineNum = 53;BA.debugLine="End Sub";
+Debug.ShouldStop(1048576);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			Debug.ErrorCaught(e);
+			throw e;
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _activity_resume() throws Exception{
+try {
+		Debug.PushSubsStack("Activity_Resume (main) ","main",0,main.mostCurrent.activityBA,main.mostCurrent,47);
+if (RapidSub.canDelegate("activity_resume")) return main.remoteMe.runUserSub(false, "main","activity_resume");
+ BA.debugLineNum = 47;BA.debugLine="Sub Activity_Resume";
+Debug.ShouldStop(16384);
+ BA.debugLineNum = 49;BA.debugLine="End Sub";
+Debug.ShouldStop(65536);
+return RemoteObject.createImmutable("");
+}
+catch (Exception e) {
+			Debug.ErrorCaught(e);
+			throw e;
+		} 
+finally {
+			Debug.PopSubsStack();
+		}}
+public static RemoteObject  _globals() throws Exception{
+ //BA.debugLineNum = 25;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 30;BA.debugLine="Dim p As Pushe";
+main.mostCurrent._p = RemoteObject.createNew ("co.ronash.pushe.b4awrapper.PusheWrapper");
+ //BA.debugLineNum = 33;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (main.processGlobalsRun == false) {
+	    main.processGlobalsRun = true;
+		try {
+		        main_subs_0._process_globals();
+starter_subs_0._process_globals();
+main.myClass = BA.getDeviceClass ("co.ronash.pushesampleb4a.main");
+starter.myClass = BA.getDeviceClass ("co.ronash.pushesampleb4a.starter");
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}public static RemoteObject  _process_globals() throws Exception{
+ //BA.debugLineNum = 19;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 23;BA.debugLine="End Sub";
+return RemoteObject.createImmutable("");
+}
+}
